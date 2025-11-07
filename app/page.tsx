9e1711b3015e57758987c013d0631761ef12dd74
imadEddineBMZ <imadeddineboumaza110@gmail.com>
2025-11-07 18:13:43 +0100

    implement the campanions cards component

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,34 @@ import React from 'react'
 import CompanionsCard from '@/components/companionsCard'
 const Page = () => {
   return (
-    <main className='bg-background min-h-screen w-full border-2 border-red-500'>
+    <main className='bg-background min-h-screen w-full'>
       <h1 className='text-2xl underline '>Popular Companions</h1>
       
       <section className='home-section'>
         <CompanionsCard 
           id='1'
+          name='Neura The Brainy Explorer'
+          topic='Neural Network Of The Brain ' 
+          subject='Science'
+          duration={45}
+          color='#ffde6e'
+        />
+        <CompanionsCard
+          id='2'
           name='countsy the number wizard'
           topic='Derivation & Integrals' 
           subject='math'
           duration={30}
           color='#e5d0ff'
         />
-        <CompanionsCard />
-        <CompanionsCard />
+        <CompanionsCard
+          id='1'
+          name='Verba The Vocabulary Builder'
+          topic='English Literature' 
+          subject='Language'
+          duration={30}
+          color='#bde7ff'
+        />
       </section>
 
 
--- components/companionsCard.tsx
+++ components/companionsCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image';
+import Link from 'next/link';
 import { Button } from './ui/button';
+
 interface CompanionsCardProps {
     id : string;
     name : string;
@@ -13,19 +15,35 @@ interface CompanionsCardProps {
 const CompanionsCard = ({ id, name, topic, subject, duration, color }: CompanionsCardProps) => {
     
   return (
-    <article className='campanion-card' style={{backgroundColor: color}}>
-        <div className='flex jutsify-between items-center '>
-            <h1 className='bg-black text-white rounded-4xl text-sm py-1 px-2 capitalize'>{subject}</h1>
-            <Button className='flex items-center h-full bg-black px-2 rounded-4xl aspect-square cursor-pointer'>
-                <Image
-                    src='/icons/bookmark.svg'
-                    alt='bookmark icon'
-                    width={12.5}
-                    height={12.5}
-                />
-            </Button>
+    <article className='companion-card' style={{backgroundColor: color}}>
+            <div className='flex items-center justify-between'>
+                <h1 className='bg-black text-white rounded-4xl text-sm py-1 px-2 capitalize'>{subject}</h1>
+                <Button className='px-2 bg-black rounded-4xl flex items-center h-full aspect-square cursor-pointer'>
+                    <Image
+                        src='/icons/bookmark.svg'
+                        alt='bookmark icon'
+                        width={12.5}
+                        height={12.5}
+                    />
+                </Button>
+            </div>
+
+        <h2 className='text-lg font-semibold '>{name}</h2>
+        <p>{topic}</p>
+        <div className='flex items-center justif-start gap-1'>
+            <Image
+                src='/icons/clock.svg'
+                alt='clock icon'
+                width={13.5}
+                height={13.5}
+            />
+            <span className='text-sm'>{duration} mins</span>
         </div>
-        <h2 className='text-lg font-semibold '>{topic}</h2>
+        <Link href={`/campanions/${id}`} >
+            <Button className='btn-primary w-full'>
+                Start Session
+            </Button>
+        </Link>
     </article>
   )
 }
